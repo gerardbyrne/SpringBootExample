6b1d5774930862b3663d2ce85e945a89bbb93117
Gerard Byrne <gerardbyrne@lineone.net>
2018-12-12 22:20:48 +0000

    Took out unused import

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
